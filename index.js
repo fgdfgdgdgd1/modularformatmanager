b9d1772343863f8b5d232be5d78cb1324606be40
fgdfgdgdgd1 <js1jikoboyz@hotmail.com>
2024-05-06 20:11:11 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const factorialRecursive = (n) => {
-  if (n === 0 || n === 1) {
-    return 1;
+function countAndSay(n) {
+  let result = "1";
+  for (let i = 1; i < n; i++) {
+    let temp = "";
+    let count = 1;
+    for (let j = 0; j < result.length; j++) {
+      if (result[j] === result[j + 1]) {
+        count++;
+      } else {
+        temp += count + result[j];
+        count = 1;
+      }
+    }
+    result = temp;
   }
-  return n * factorialRecursive(n - 1);
-};
+  return result;
+}
