1c69a14f7013925028963a5c5a6403ff09fab946
fgdfgdgdgd1 <js1jikoboyz@hotmail.com>
2024-05-14 20:39:42 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-const senderSignature = aptos.transaction.sign({ signer: alice, transaction });
-
-// Sponsor signs
-const sponsorSignature = aptos.transaction.signAsFeePayer({
-  signer: sponsor,
-  transaction,
-});
-
-// Submit the transaction to chain
-const committedTxn = await aptos.transaction.submit.simple({
-  transaction,
-  senderAuthenticator: senderSignature,
-  feePayerAuthenticator: sponsorSignature,
-});
+const randomNumber = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
